3fb110a1345e9c9601b60674e1d2faf6fe87a698
JayJay Zepeda <jayjayzepeda@JayJays-MacBook-Pro.local>
2018-09-23 20:18:59 -0700

    finish assignment 2

--- main.ts
+++ main.ts
@@ -1,22 +1,24 @@
 
 
-class MyFirstProgram {
-  constructor(...argArray: Array<any>) {
-    this.printArgs(argArray);
+class Greeter {
+
+  constructor(private first_name: string, private last_name: string) {
   }
-  printArgs(argArray) {
-    console.log(argArray);
-  }
-  runFirst(callback: Function) {
-    console.log('this method runs first!...');
-  }
-  rundLast() {
-    console.log('this method runs last!');
-  }
-  runMiddle() {
-    console.log('this code run in between the other two functions...!');
-  }
+
+
+showGreeting() {
+  const banner = document.getElementById('banner');
+  banner.innerHTML = 'Hello ' + this.first_name + ' ' + this.last_name + '.';
 }
 
-const newInstance = new MyFirstProgram("a", "b", "c");
+hideGreeting() {
+  const banner = document.getElementById('banner');
+  banner.innerHTML = 'r1c2';
+
+}
+
+}
+
+const greeter = new Greeter('Jonathan', 'Zepeda');
+
 
